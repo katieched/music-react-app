f3e4823cb48269c7a005d878d495563b97e3d62c
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-11-01 15:07:59 +0000

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Artists } from './components/artists';
+
+const App = () => {
+    return (
+        <>
+            <h1>Favourite Artists</h1>
+        </>
+    )
+};
+
+export default App;
---
+++ src/components/artists.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Artists = [
+    { name: 'ABBA', musicGenre: 'Pop', intro: 'ABBA are a Swedish pop group formed in Stockholm in 1972 by Agnetha Fältskog, Björn Ulvaeus, Benny Andersson, and Anni-Frid Lyngstad.'},
+    { name: 'Queen', musicGenre: 'Rock', intro: 'Queen are a British rock band formed in London in 1970. Their classic line-up was Freddie Mercury, Brian May, Roger Taylor and John Deacon.'}
+]
+
+export default Artists;
